a9faecd112f3d06d7612576d7d5f75ede33a1f52
Pasindumi <139098592+Pasindumi@users.noreply.github.com>
2025-01-14 07:31:01 -0800

    main

--- csq/src/App.js
+++ csq/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddLecturer from "./components/AddLecturer";
+import Sidebar from "./components/Sidebar";
+import "./App.css"; // Add global styles here
 
 function App() {
   return (
-    <div>
-      <AddLecturer />
-    </div>
+    <Router>
+      <div className="app-layout">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/add-lecturer" element={<AddLecturer />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
