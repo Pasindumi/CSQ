57174f006b185105f0133bec09123f81483e115c
Pasindumi <139098592+Pasindumi@users.noreply.github.com>
2025-01-16 05:47:15 -0800

    forms

--- csq/src/components/Forms.jsx
+++ csq/src/components/Forms.jsx
@@ -9,56 +9,67 @@ function Forms() {
         <div className="search-bar">
           <input type="text" placeholder="Search" />
         </div>
-        
       </header>
       <hr className='xyz' />
       <section className="new-forms">
-      <h2>New forms</h2>
+        <h2>New forms</h2>
         <div className="form-templates">
           <div className="form-cards">
-          <img src="pluss.png" alt="Blank Form Icon" className="form-icon"/>
+            <img src="pluss.png" alt="Blank Form Icon" className="form-icon" />
             <p>Create New form</p>
           </div>
           <div className="form-card">
-          <img src="lectures.jpg" alt="Blank Form Icon" className="form-icon"/>
+            <img src="lectures.jpg" alt="Blank Form Icon" className="form-icon" />
             <p>Lecturer Reviews</p>
           </div>
           <div className="form-card">
-          <img src="env.jpg" alt="Blank Form Icon" className="form-icons"/>
+            <img src="env.jpg" alt="Blank Form Icon" className="form-icons" />
             <p>Learning Environment</p>
           </div>
           <div className="form-card">
-          <img src="support.jpg" alt="Blank Form Icon" className="form-icons"/>
+            <img src="support.jpg" alt="Blank Form Icon" className="form-icons" />
             <p>Support Services</p>
           </div>
           <div className="form-card">
-          <img src="methids.jpg" alt="Blank Form Icon" className="form-icons"/>
-            <p>Teachig Methods</p>
+            <img src="methids.jpg" alt="Blank Form Icon" className="form-icons" />
+            <p>Teaching Methods</p>
           </div>
         </div>
       </section>
-<hr className='abc' />
-      
+      <hr className='abc' />
+
       <section className="recent-forms">
         <h2>Recent forms</h2>
         <div className="form-templates">
           <div className="form-card-recent">
-            <div className="form-icon recent-form"></div>
-            <p>Super fun event registration</p>
-            <span>Opened 12:49 PM</span>
+            <div className="form-icon recent-form">
+              <img src="cinec.jpg" alt="Event Icon" className="recent-form-img" />
+              <img src="formsicon.webp" alt="Hovered Event Icon" className="recent-form-hover-img" />
+            </div>
+            <p>CINEC CSSQ Form  FI12038</p>
+            <span>01.12.2024</span>
           </div>
           <div className="form-card-recent">
-            <div className="form-icon recent-form"></div>
+            <div className="form-icon recent-form">
+              <img src="cinec.jpg" alt="Event Icon" className="recent-form-img" />
+              <img src="formsicon.webp" alt="Hovered Event Icon" className="recent-form-hover-img" />
+            </div>
             <p>Event Feedback</p>
             <span>Opened 10:57 AM</span>
           </div>
           <div className="form-card-recent">
-            <div className="form-icon recent-form"></div>
+            <div className="form-icon recent-form">
+              <img src="cinec.jpg" alt="Event Icon" className="recent-form-img" />
+              <img src="formsicon.webp" alt="Hovered Event Icon" className="recent-form-hover-img" />
+            </div>
             <p>Demo order form</p>
             <span>Opened 10:43 AM</span>
           </div>
           <div className="form-card-recent">
-            <div className="form-icon recent-form"></div>
+            <div className="form-icon recent-form">
+              <img src="cinec.jpg" alt="Event Icon" className="recent-form-img" />
+              <img src="formsicon.webp" alt="Hovered Event Icon" className="recent-form-hover-img" />
+            </div>
             <p>Demo team event poll</p>
             <span>Opened 10:40 AM</span>
           </div>
