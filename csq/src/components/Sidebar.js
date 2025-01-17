4d1737cddd1a5fdbda82832f5e397601509a7655
Pasindumi <139098592+Pasindumi@users.noreply.github.com>
2025-01-17 00:28:37 -0800

    al

--- csq/src/components/Sidebar.js
+++ csq/src/components/Sidebar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< Updated upstream
 import "../styles/SidebarStyles.css";
 
 import dashicon from "../components/assets/dash.png";
@@ -13,16 +12,14 @@ import lectureicon from "../components/assets/lecture.png";
 import settingicon from "../components/assets/setting.png";
 import loginicon from "../components/assets/login.png";
 import addicon from "../components/assets/add.png";
-=======
-import "../styles/SidebarStyles.css"; // Add your sidebar styles here
->>>>>>> Stashed changes
+
 
 const Sidebar = () => {
   return (
+    <div>
     <div className="sidebar">
       <ul className="sidebar-menu">
         <li>
-<<<<<<< Updated upstream
           <Link to="/dashboard">
             <img src={dashicon} alt="Dashboard" /> Dashboard
           </Link>
@@ -72,45 +69,16 @@ const Sidebar = () => {
             <img src={addicon} alt="Add-Lecturer" /> Add Lecturer
           </Link>
         </li>
-      </ul>
-=======
-          <Link to="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/forms">Forms</Link>
-        </li>
-        <li>
-          <Link to="/results">Results</Link>
-        </li>
-        <li>
-          <Link to="/faculties">Faculties</Link>
-        </li>
-        <li>
-          <Link to="/lecturers">Lecturers</Link>
-        </li>
-        <li>
-          <Link to="/user-management">User Management</Link>
-        </li>
-        <li>
-          <Link to="/settings">Settings</Link>
-        </li>
-        <li>
-  <Link to="/add-lecturer">Add Lecturer</Link>
-</li>
+     
 </ul>
->>>>>>> Stashed changes
+</div> 
       <div className="logout-button">
         <button onClick={() => alert("Logging out...")}>Logout</button>
       </div>
-    </div>
+
+      </div>
+     
   );
 };
 
-<<<<<<< Updated upstream
 export default Sidebar;
-=======
-export default Sidebar;
->>>>>>> Stashed changes
