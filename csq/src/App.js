2233e5fb1e0fd4ad2e95ac989665fe040c05a1b6
Pasindumi <139098592+Pasindumi@users.noreply.github.com>
2025-01-17 00:03:02 -0800

    all

--- csq/src/App.js
+++ csq/src/App.js
@@ -3,7 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddLecturer from "./components/AddLecturer";
 import Sidebar from "./components/Sidebar";
 import Forms from './components/Forms';
+<<<<<<< Updated upstream
 import Createform from './components/createform';
+=======
+import "./App.css"; 
+import LoginSignup from "./components/LoginSignup";
+>>>>>>> Stashed changes
 import "./App.css"; 
 
 import "./App.css";
--- csq/src/components/Sidebar.js
+++ csq/src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+<<<<<<< Updated upstream
 import "../styles/SidebarStyles.css";
 
 import dashicon from "../components/assets/dash.png";
@@ -12,12 +13,16 @@ import lectureicon from "../components/assets/lecture.png";
 import settingicon from "../components/assets/setting.png";
 import loginicon from "../components/assets/login.png";
 import addicon from "../components/assets/add.png";
+=======
+import "../styles/SidebarStyles.css"; // Add your sidebar styles here
+>>>>>>> Stashed changes
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <ul className="sidebar-menu">
         <li>
+<<<<<<< Updated upstream
           <Link to="/dashboard">
             <img src={dashicon} alt="Dashboard" /> Dashboard
           </Link>
@@ -68,6 +73,35 @@ const Sidebar = () => {
           </Link>
         </li>
       </ul>
+=======
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/forms">Forms</Link>
+        </li>
+        <li>
+          <Link to="/results">Results</Link>
+        </li>
+        <li>
+          <Link to="/faculties">Faculties</Link>
+        </li>
+        <li>
+          <Link to="/lecturers">Lecturers</Link>
+        </li>
+        <li>
+          <Link to="/user-management">User Management</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+        <li>
+  <Link to="/add-lecturer">Add Lecturer</Link>
+</li>
+</ul>
+>>>>>>> Stashed changes
       <div className="logout-button">
         <button onClick={() => alert("Logging out...")}>Logout</button>
       </div>
@@ -75,4 +109,8 @@ const Sidebar = () => {
   );
 };
 
+<<<<<<< Updated upstream
 export default Sidebar;
+=======
+export default Sidebar;
+>>>>>>> Stashed changes
